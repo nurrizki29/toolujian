f0cd72c8e88b8448266649fc40c40d37e9b72790
Mohammad Nur Rizki <mnurrizki25@gmail.com>
2021-11-22 10:14:17 +0700

    feat(ReCaptcha): Add ReCaptcha

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,8 +11,10 @@ import {mahasiswa} from "../database/datainduk.json"
 import LoadingPage from '../components/LoadingPage';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+import ReCAPTCHA from "react-google-recaptcha";
 
 function Login() {
+    const recaptchaRef = React.useRef();
     const cookies = new Cookies();
     //define state
     const [npm, setNPM] = useState("");
@@ -59,12 +61,15 @@ function Login() {
     const classNormal = 'text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none '
     const classError = 'border-red-500'
     const loginHandler = async (e) => {
+        recaptchaRef.current.reset()
         document.getElementById("btnLogin").disabled = true
         e.preventDefault();
         await fadeIn()
+        const token = await recaptchaRef.current.executeAsync();
         let filtered = mahasiswa.filter(function(entry){
           return entry.npm === npm
         })
+        console.log(filtered)
         if (filtered.length > 0){
           cookies.set('data', JSON.stringify(filtered[0]), { path: '/',maxAge:5*24*60*60 })
           //redirect to dashboard
@@ -75,6 +80,7 @@ function Login() {
         }    
         fadeOut()
     };
+    
     return (
         <div className="container mr-auto ml-auto">
             <LoadingPage />
@@ -93,15 +99,21 @@ function Login() {
                 )
             }
           </div>
-          <div>
-            <button type="submit" className="block w-full mt-8 bg-blue-500 p-2 rounded text-white font-bold hover:bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed" id="btnLogin">
+          <ReCAPTCHA
+            ref={recaptchaRef}
+            size="invisible"
+            sitekey="6Lc4Yk4dAAAAALkQz0sa3yWqTYunQtGddmFLjrZU"
+          />
+          <div className="mt-8 text-center text-gray-500 text-sm">
+            <button type="submit" className="block w-full mb-2 bg-blue-500 p-2 rounded text-white font-bold hover:bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed text-md" id="btnLogin">
               <FontAwesomeIcon icon={faSignInAlt} /> Sign in
             </button>
+            This site is protected by <span className='font-medium'>reCAPTCHA</span> and the Google <span className='font-medium'><a href="https://policies.google.com/privacy">Privacy Policy</a></span> and <span className='font-medium'><a href="https://policies.google.com/privacy">Terms of Service</a></span> apply.
           </div>
         </form>
-        <div className='mt-8 text-center text-gray-700'>
+        <div className='mt-4 text-center text-gray-700'>
             Made with ❤️ to help make your life easier
-            <div className='text-gray-700 text-sm mt-2'>v.1.0.3</div>
+            <div className='text-gray-700 text-sm mt-2'>v.1.0.4</div>
         </div>
       </div>{
 
